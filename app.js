b0a05f133d7ec7421e6460f1eb9f49cc36993b06
Shandy Sulen <shandysulen@ufl.edu>
2018-02-07 17:25:52 -0500

    added colors to the console messages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const net = require('net');
+var colors = require('colors');
 
 var server = net.createServer();
 
@@ -7,26 +8,26 @@ var port = 9000;
 /* TCP connection - different from HTTP request/response model */
 server.on("connection", (socket) => {
     var remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
-    console.log("New client connection has been made at %s", remoteAddress);
+    console.log("New client connection has been made at %s".green, remoteAddress);
 
     socket.on("data", (data) => {
-        console.log("Data from %s: %s", remoteAddress, data);
+        console.log("Data from %s: %s".cyan, remoteAddress, data);
         socket.write("Thanks!");
     });
 
     socket.once("close", () => {
-        console.log("Connection with %s closed", remoteAddress);
+        console.log("Connection with %s closed".yellow, remoteAddress);
     });
 
     socket.on("error", () => {
-        console.log("Connection error with %s", remoteAddress);
+        console.log("Connection error with %s".red, remoteAddress);
     });
 
 });
 
 server.on('error', (e) => {
   if (e.code === 'EADDRINUSE') {
-    console.log('Address in use, restarting server...');
+    console.log('Address in use, restarting server...'.yellow);
     setTimeout( () => {
       server.close();
       port++;
@@ -36,5 +37,5 @@ server.on('error', (e) => {
 });
 
 server.listen(port, () => {
-    console.log("Server listening on %j", server.address().port);
+    console.log("Server listening on %j".green, server.address().port);
 });
